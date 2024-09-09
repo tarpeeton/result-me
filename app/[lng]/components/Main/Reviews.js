9e8815073e1192f21b440bb7d779ed93560f842c
tarpeeton <146853258+tarpeeton@users.noreply.github.com>
2024-09-09 13:53:57 +0500

    Reviews Mobile

--- app/[lng]/components/Main/Reviews.js
+++ app/[lng]/components/Main/Reviews.js
@@ -59,12 +59,12 @@ const handleNext = () => {
 
   return (
     <div
-      className="relative w-full mt-[20px] mdl:mt-[30px] rounded-[30px] bg-cover bg-no-repeat px-5 py-8"
+      className="relative w-full mt-[20px] mdl:mt-[30px] rounded-[30px] bg-cover bg-no-repeat px-5 py-8 bg-[#161616] bg-opacity-[90%]"
       style={{ backgroundImage: `url(${revBG.src})` }}
     >
       {/* Top Decorative Image */}
-      <div className="w-[80%] absolute top-0 right-0 rounded-[30px]">
-        <Image width={400} height={400} src={revTop} alt="revTop"  className='rounded-[30px]'/>
+      <div className="w-[80%] absolute top-0 right-0 rounded-[30px] ">
+        <Image width={400} height={400} src={revTop} alt="revTop"  className='rounded-[30px] '/>
       </div>
 
       {/* Reviews Title */}
@@ -72,17 +72,17 @@ const handleNext = () => {
 
       {/* Reviews Carousel */}
       <Slider {...settings} ref={sliderRef} className='z-[99999]'>
-        <div className="rounded-xl p-4 bg-[#161616] bg-opacity-[50%]  text-white shadow-md">
-          <h3 className="text-[18px] font-bold mb-2">Матьякуб Назарович</h3>
+        <div className="rounded-[30px] p-4 bg-[#161616] bg-opacity-[50%]  text-white  z-[99999] min-h-[400px] border border-[#5A5A5A]">
+          <h3 className="text-[18px] font-semibold mb-2">Матьякуб Назарович</h3>
           <p className="text-[14px] opacity-75 mb-4">Главврач Intermed</p>
-          <p className="text-[14px]">
+          <p className="text-[15px] leading-[18px]">
             Отличная работа! Команда профессионалов создала для нас сайт, который идеально отражает нашу клинику и помогает привлекать новых пациентов.
           </p>
         </div>
-        <div className="rounded-xl p-4 bg-[#161616]  bg-opacity-[50%] text-white shadow-md">
-          <h3 className="text-[18px] font-bold mb-2">Матьякуб Назарович</h3>
+        <div className="rounded-[30px] p-4 bg-[#161616]  bg-opacity-[50%] text-white  min-h-[400px] border border-[#5A5A5A]">
+          <h3 className="text-[18px] font-semibold mb-2">Матьякуб Назарович</h3>
           <p className="text-[14px] opacity-75 mb-4">Главврач Intermed</p>
-          <p className="text-[14px]">
+          <p className="text-[15px] leading-[18px]">
             Отличная работа! Команда профессионалов создала для нас сайт, который идеально отражает нашу клинику и помогает привлекать новых пациентов.
           </p>
         </div>
@@ -95,11 +95,11 @@ const handleNext = () => {
       </div>
 
       {/* Call to Action Button */}
-      <div className="text-center mt-6 flex items-center z-[99999]">
-        <button className="bg-white text-black font-semibold py-2 px-6 rounded-full hover:bg-gray-200 transition">
+      <div className="text-center mt-6 flex items-center">
+        <button className="w-[80%] mx-auto py-[20px] px-[30px] bg-white text-violet100 font-bold text-[14px]  rounded-full hover:bg-gray-200 transition z-[99999]">
           Стать нашим клиентом
         </button>
-		<div className='sm:hidden flex flex-row gap-[8px] 2xl:flex'>
+		<div className='sm:hidden flex flex-row gap-[8px] 2xl:flex z-[9999]'>
 					<SamplePrevArrow onClick={handlePrev} />
 					<SampleNextArrow onClick={handleNext} />
 				</div>
