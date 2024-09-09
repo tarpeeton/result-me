aaae62ca19c7acaffb339985d5f26bd333192de9
tarpeeton <146853258+tarpeeton@users.noreply.github.com>
2024-09-09 11:18:31 +0500

    Added Winner Animation

--- app/[lng]/components/Main/Puzzle.js
+++ app/[lng]/components/Main/Puzzle.js
@@ -10,10 +10,13 @@ import Three from '@/public/images/puzzle/Part3.png';
 import Four from '@/public/images/puzzle/Part4.png';
 import Five from '@/public/images/puzzle/Part5.png';
 import Six from '@/public/images/puzzle/Part6.png';
+import Winner from '@/public/images/puzzle/winner.png';
+import arrowWinner from '@/public/images/puzzle/arrowWinner.png';
+import Link from 'next/link'
 
 const PuzzlePiece = ({ id, imagePart, onClick, rotation }) => {
   useEffect(() => {
-    // GSAP entry animation
+    // GSAP entry animation for each piece
     gsap.fromTo(`.piece-${id}`, { opacity: 0, scale: 0.5 }, { opacity: 1, scale: 1, duration: 1, delay: id * 0.2 });
   }, [id]);
 
@@ -30,56 +33,86 @@ const PuzzlePiece = ({ id, imagePart, onClick, rotation }) => {
   );
 };
 
+const WinnerInfo = () => {
+  useEffect(() => {
+    // GSAP animation for WinnerInfo appearance
+    const timeline = gsap.timeline();
+    timeline
+      .fromTo('.winner-container', { opacity: 0, scale: 0.8 }, { opacity: 1, scale: 1, duration: 1, ease: 'power3.out' })
+      .fromTo('.winner-text', { y: 100, opacity: 0 }, { y: 0, opacity: 1, duration: 1, ease: 'power3.out' }, '-=0.5')
+      .fromTo('.winner-button', { y: 50, opacity: 0 }, { y: 0, opacity: 1, duration: 0.7, ease: 'power3.out' }, '-=0.3')
+      .fromTo('.winner-arrow', { x: -50, opacity: 0 }, { x: 0, opacity: 1, duration: 0.7, ease: 'power3.out' }, '-=0.7');
+  }, []);
+
+  return (
+    <div className='winner-container flex flex-col w-full h-full rounded-[30px] bg-violet100 relative overflow-hidden pb-[30px]'>
+      <div className='relative overflow-hidden h-[116px]'>
+        <Image src={Winner} width={116} height={116} quality={100} alt='Winner Image' className='absolute top-0 right-[-50px]'/>
+      </div>
+      <div className='flex flex-col gap-[8px] mdl:gap-[10px] 4xl:gap-[30px] w-[80%] mx-auto'>
+        <div className='text-center'>
+          <p className='winner-text text-[12px] leading-[19px] text-white100 mdl:text-[18px] mdl:leading-[28px] 4xl:text-[25px] 4xl:leading-[30.48px]'>Поздравляем! Вы получили подарок</p>
+        </div>
+        <div className='text-center'>
+          <h2 className='winner-text text-[28px] font-bold text-white100 leading-[34.13px] mdl:text-[40px] mdl:leading-[48.76px] 4xl:text-[90px] 4xl:leading-[109.71px]'>БЕСПЛАТНЫЙ РАЗБОР ВАШЕГО ПРОЕКТА</h2>
+        </div>
+      </div>  
+      <div className='relative w-[70px] h-[70px]'>
+        <Image src={arrowWinner} width={400} height={400} quality={100} alt='Winner Image' className='winner-arrow absolute left-[20px] top-[-20px]'/>
+      </div>
+      <div className='winner-button bg-white100 w-[70%] mx-auto py-[20px] px-[30px] rounded-[100px] text-center'>
+        <Link href='zayafka' className=' text-violet100 text-[14px] font-bold mdl:text-[18px]'> 
+          Связаться с нами
+        </Link>
+      </div>
+    </div>
+  );
+};
+
 const Puzzle = () => {
   const [rotations, setRotations] = useState({});
+  const [win , setWin] = useState(false)
 
-  // Correct rotation steps for each piece
   const correctRotations = {
-    1: 180, // 2 clicks -> 180°
-    2: 270, // 3 clicks -> 270°
-    3: 90,  // 1 click  -> 90°
-    4: 270, // 3 clicks -> 270°
-    5: 180, // 2 clicks -> 180°
-    6: 270  // 3 clicks -> 270°
+    1: 180,
+    2: 270,
+    3: 90,
+    4: 270,
+    5: 180,
+    6: 270
   };
 
-  // Initialize rotations for each piece
   useEffect(() => {
     const initialRotations = [1, 2, 3, 4, 5, 6].reduce((acc, piece) => {
-      acc[piece] = 0; // Start with 0 degrees
+      acc[piece] = 0;
       return acc;
     }, {});
     setRotations(initialRotations);
   }, []);
 
-  // Handle piece click and rotate
   const handleClick = (id) => {
     const newRotation = (rotations[id] + 90) % 360;
 
-    // Apply GSAP rotation with animation
     gsap.to(`.piece-${id}`, { duration: 0.5, rotate: newRotation });
 
     setRotations((prevRotations) => ({
       ...prevRotations,
-      [id]: newRotation // Update rotation state
+      [id]: newRotation
     }));
   };
 
-  // Check if puzzle is solved
   const checkIfSolved = () => {
     const allCorrect = [1, 2, 3, 4, 5, 6].every((piece) => rotations[piece] === correctRotations[piece]);
 
     if (allCorrect) {
-      console.log("Welcome");
+      setWin(true);
     }
   };
 
-  // Check if puzzle is solved every time rotations change
   useEffect(() => {
     checkIfSolved();
   }, [rotations]);
 
-  // Directly use imported images for the image map
   const imageMap = {
     1: One,
     2: Two,
@@ -90,22 +123,24 @@ const Puzzle = () => {
   };
 
   return (
-    <div className="flex px-[12px] py-[30px] flex-col items-center justify-center bg-violet100 rounded-[30px] mdl:rounded-[60px] 4xl:rounded-[100px] mt-[20px] mdl:mt-[25px] 4xl:mt-[30px] 4xl:py-[80px]">
-      <h1 className="text-white text-[28px] mx-auto w-[90%] mdl:text-[40px] 4xl:text-[50px] mb-4 font-bold">Соберите пазл и получите подарок!</h1>
-      
-      {/* Grid layout with puzzle pieces placed in order */}
-      <div className="grid grid-cols-3 grid-rows-2 gap-0 w-full max-w-[600px] mdl:max-w-[800px] 4xl:max-w-[1200px]">
-        {/* First row */}
-        <PuzzlePiece id={1} imagePart={imageMap[1]} onClick={handleClick} rotation={rotations[1]} />
-        <PuzzlePiece id={2} imagePart={imageMap[2]} onClick={handleClick} rotation={rotations[2]} />
-        <PuzzlePiece id={3} imagePart={imageMap[3]} onClick={handleClick} rotation={rotations[3]} />
-        {/* Second row */}
-        <PuzzlePiece id={4} imagePart={imageMap[4]} onClick={handleClick} rotation={rotations[4]} />
-        <PuzzlePiece id={5} imagePart={imageMap[5]} onClick={handleClick} rotation={rotations[5]} />
-        <PuzzlePiece id={6} imagePart={imageMap[6]} onClick={handleClick} rotation={rotations[6]} />
-      </div>
-      <p className="text-white font-semibold mt-[30px]  4xl:mt-[80px] text-[14px] mdl:text-[20px] 4xl:text-[25px]">Не пролистывайте просто так :)</p>
-    </div>
+    <>
+      {win ? (
+        <WinnerInfo />
+      ) : (
+        <div className="flex px-[12px] py-[30px] flex-col items-center justify-center bg-violet100 rounded-[30px] mdl:rounded-[60px] 4xl:rounded-[100px] mt-[20px] mdl:mt-[25px] 4xl:mt-[30px] 4xl:py-[80px]">
+          <h1 className="text-white text-[28px] mx-auto w-[90%] mdl:text-[40px] 4xl:text-[50px] mb-4 font-bold">Соберите пазл и получите подарок!</h1>
+          <div className="grid grid-cols-3 grid-rows-2 gap-0 w-full max-w-[600px] mdl:max-w-[800px] 4xl:max-w-[1200px]">
+            <PuzzlePiece id={1} imagePart={imageMap[1]} onClick={handleClick} rotation={rotations[1]} />
+            <PuzzlePiece id={2} imagePart={imageMap[2]} onClick={handleClick} rotation={rotations[2]} />
+            <PuzzlePiece id={3} imagePart={imageMap[3]} onClick={handleClick} rotation={rotations[3]} />
+            <PuzzlePiece id={4} imagePart={imageMap[4]} onClick={handleClick} rotation={rotations[4]} />
+            <PuzzlePiece id={5} imagePart={imageMap[5]} onClick={handleClick} rotation={rotations[5]} />
+            <PuzzlePiece id={6} imagePart={imageMap[6]} onClick={handleClick} rotation={rotations[6]} />
+          </div>
+          <p className="text-white font-semibold mt-[30px] 4xl:mt-[80px] text-[14px] mdl:text-[20px] 4xl:text-[25px]">Не пролистывайте просто так :)</p>
+        </div>
+      )}
+    </>
   );
 };
 
