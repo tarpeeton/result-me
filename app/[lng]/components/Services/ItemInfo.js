5f8e4d55355333d0737794bd203eb936d44c839e
tarpeeton <146853258+tarpeeton@users.noreply.github.com>
2024-09-10 14:00:40 +0500

    Item Info

---
+++ app/[lng]/components/Services/ItemInfo.js
@@ -0,0 +1,18 @@
+
+
+
+
+const ItemInfo = () => {
+  return (
+	<div className='mt-[20px] mdl:mt-[20px] 3xl:mt-[25px] rounded-[30px] mdl:rounded-[40px]  flex flex-col 3xl:flex-row py-[30px] px-[24px] mdl:py-[50px] mdl:px-[40px] 3xl:py-[80px] 3xl:px-[70px] bg-white mx-[16px] mdl:mx-[20px] 3xl:mx-[30px]'>
+		<div className='2xl:w-[50%]'>
+			<h2 className='text-[28px] 2xl:w-[80%] 4xl:w-[60%] mdl:text-[40px] 3xl:text-[50px]  font-bold'>Ваш успех начинается здесь</h2>
+		</div>
+		<div className='mt-[20px] mdl:mt-[30px] 3xl:mt-0 2xl:order-1 2xl:w-[50%]'>
+			<p className='text-[15px] mdl:text-[20px]  font-medium'>Мы предлагаем комплексные услуги по созданию сайтов, начиная с анализа бизнеса и разработки стратегии, заканчивая дизайном, программированием и запуском. Наши сайты адаптированы под любые устройства и легко масштабируются. Мы также обеспечиваем интеграцию с различными системами и SEO-оптимизацию, чтобы ваш сайт привлекал целевую аудиторию и способствовал росту вашего бизнеса</p>
+		</div>
+	</div>
+  );
+};
+
+export default ItemInfo;
--- app/[lng]/services/[slug]/page.js
+++ app/[lng]/services/[slug]/page.js
@@ -1,9 +1,13 @@
-
+import ServiceItem from "@/app/[lng]/components/Services/Item";
+import ItemInfo from "@/app/[lng]/components/Services/ItemInfo";
 
 const Service = () => {
   return (
-	<div>
-		Salom
+	<div className='bg-[#F8F8F8] relative'>
+		<div className='pt-[20px] mdl:pt-[30px]'>
+		<ServiceItem/>
+		<ItemInfo/>
+		</div>
 	</div>
   );
 };
