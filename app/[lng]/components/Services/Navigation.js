f4d96a5443333a1e616a0488bfff0b11b33a1c60
tarpeeton <146853258+tarpeeton@users.noreply.github.com>
2024-09-17 12:10:51 +0500

    Service Navigation updated

--- app/[lng]/components/Services/Navigation.js
+++ app/[lng]/components/Services/Navigation.js
@@ -1,59 +1,33 @@
-'use client';
-import { useState, useEffect, useRef } from 'react';
-import { gsap } from 'gsap';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+"use client";
+import { useState, useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import Link from 'next/link';
 
 const Navigation = () => {
-  const [activeNav, setActiveNav] = useState('Сайты');
+  const [activeNav, setActiveNav] = useState("web-development");  // Change to use slugs for activeNav
   const navRefs = useRef([]);  // Array to store refs for `a` elements
   const spanRefs = useRef([]); // Array to store refs for `span` elements
 
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 900,
-    slidesToShow: 4, 
-    slidesToScroll: 1,
-    initialSlide: 0,
-    arrows: false,
-    centerPadding: '40px',
-    responsive: [
-      {
-        breakpoint: 1024, 
-        settings: {
-          slidesToShow: 3,
-          centerPadding: '40px',
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 768, 
-        settings: {
-          slidesToShow: 3,
-          centerPadding: '40px',
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 480, 
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          centerPadding: '40px'
-        },
-      },
-    ],
+  const responsive = {
+    tablet: {
+      breakpoint: { max: 1280, min: 768 },
+      items: 4,
+    },
+    mobile: {
+      breakpoint: { max: 768, min: 0 },
+      items: 3,
+    },
   };
 
   const navItems = [
-    { name: 'Сайты',  slug: 'web-development' },
-    { name: 'Telegram-боты', slug: 'telegram-bot-development' },
-    { name: 'SMM', href: 'smm', slug: 'smm' },
-    { name: 'Реклама',  slug: 'ads-launch' },
-    { name: 'SEO', slug: 'seo' },
-    { name: 'Брендинг', slug: 'branding' },
+    { name: "Сайты", slug: "web-development" },
+    { name: "Telegram-боты", slug: "telegram-bot-development" },
+    { name: "SMM", slug: "smm" },
+    { name: "Реклама", slug: "ads-launch" },
+    { name: "SEO", slug: "seo" },
+    { name: "Брендинг", slug: "branding" },
   ];
 
   const updateSpanWidths = () => {
@@ -68,8 +42,8 @@ const Navigation = () => {
   useEffect(() => {
     // Ensure the span width matches the a element width on initial load and window resize
     updateSpanWidths();
-    window.addEventListener('resize', updateSpanWidths);
-    return () => window.removeEventListener('resize', updateSpanWidths);
+    window.addEventListener("resize", updateSpanWidths);
+    return () => window.removeEventListener("resize", updateSpanWidths);
   }, []);
 
   useEffect(() => {
@@ -78,26 +52,30 @@ const Navigation = () => {
       const spanRef = spanRefs.current[index];
       if (ref && spanRef) {
         const aWidth = ref.offsetWidth;
-        if (navItems[index].name === activeNav) {
-          gsap.to(spanRef, { width: aWidth, opacity: 1, marginTop: '5px', duration: 0.3, ease: 'power2.out' });
+        if (navItems[index].slug === activeNav) {
+          gsap.to(spanRef, { width: aWidth, opacity: 1, marginTop: "10px", duration: 0.3, ease: "power2.out" });
         } else {
-          gsap.to(spanRef, { width: '0%', opacity: 0, duration: 0.1 });
+          gsap.to(spanRef, { width: "0%", opacity: 0, duration: 0.1 });
         }
       }
     });
   }, [activeNav]);
 
+  const handleSelect = (slug) => {
+    setActiveNav(slug);
+  };
+
   return (
     <>
-      {/* FOR DESKTOP VIEW */}
+      {/* Desktop Navigation */}
       <nav className="hidden 2xl:flex px-[20px] gap-[50px] border-t border-t-[#F0F0F0] 2xl:items-center">
         {navItems.map((item, index) => (
-          <div key={item.name} className="flex flex-col items-center relative justify-center text-center pt-[25px]">
+          <div key={item.slug} className="flex flex-col items-center relative justify-center text-center pt-[25px]">
             <a
               ref={(el) => (navRefs.current[index] = el)} // Ref for a element
               href={`/ru/services/${item.slug}`}
-              className={`text-[20px] font-semibold ${activeNav === item.name ? 'text-[#7B72EB]' : 'text-black'} hover:text-[#7B72EB] pb-1`}
-              onClick={() => setActiveNav(item.name)}
+              className={`text-[20px] font-semibold ${activeNav === item.slug ? 'text-[#7B72EB]' : 'text-black'} hover:text-[#7B72EB] pb-1`}
+              onClick={() => handleSelect(item.slug)}
               style={{ whiteSpace: 'nowrap', textOverflow: 'ellipsis', overflow: 'hidden' }}
             >
               {item.name}
@@ -111,28 +89,35 @@ const Navigation = () => {
         ))}
       </nav>
 
-      {/* FOR MOBILE AND TABLET VIEW */}
-      <nav className=" 2xl:hidden py-[20px] px-[16px]">
-      <Slider {...settings}>
-        {navItems.map((item, index) => (
-          <div key={item.name} className="flex flex-col items-center relative px-2">
-            <a
-              ref={el => (navRefs.current[index] = el)}
-              href={`/ru/services/${item.slug}`}
-              className={`text-sm font-semibold ${activeNav === item.name ? 'text-[#7B72EB]' : 'text-black'} hover:text-[#7B72EB] pb-1`}
-              onClick={() => setActiveNav(item.name)}
-            >
-              {item.name}
-            </a>
-            <span
-              ref={el => (spanRefs.current[index] = el)}
-              className="absolute bottom-[-10px] h-1 bg-[#7B72EB] rounded"
-              style={{ width: 0 }}
-            ></span>
-          </div>
-        ))}
-      </Slider>
-    </nav>
+      {/* Mobile Carousel Navigation */}
+      <div className="2xl:hidden w-full  pt-[20px]">
+        <Carousel
+          responsive={responsive}
+          arrows={false}
+          showDots={false}
+          infinite={false}
+        >
+          {navItems.map((item, index) => (
+            <div key={item.slug} className="text-center">
+              <Link
+                href={`/ru/services/${item.slug}`}
+                className={`text-[15px] pb-[10px] font-semibold ${
+                  activeNav === item.slug ? "text-violet100" : "text-titleDark"
+                }`}
+                onClick={() => handleSelect(item.slug)}
+              >
+                {item.name}
+              </Link>
+              {activeNav === item.slug && (
+                <span
+                  ref={(el) => (spanRefs.current[index] = el)} // Ref for span element
+                  className="block w-[80%] h-[5px] mt-[20px] bg-violet100 mx-auto rounded-t-[5px]"
+                />
+              )}
+            </div>
+          ))}
+        </Carousel>
+      </div>
     </>
   );
 };
