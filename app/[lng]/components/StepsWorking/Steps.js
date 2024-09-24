2d3d8a7a5159249f1047c3891dcf0fccbe5d8b3f
tarpeeton <146853258+tarpeeton@users.noreply.github.com>
2024-09-24 12:42:38 +0500

    Update

--- app/[lng]/components/StepsWorking/Steps.js
+++ app/[lng]/components/StepsWorking/Steps.js
@@ -114,70 +114,76 @@ const data = [
 const Steps = () => {
 	return (
 		<div className='relative'>
-			<div className='flex flex-col'>
+			<div className='flex flex-col 2xl:flex-row 2xl:flex-wrap'>
 				{/* CRAD */}
 				{data.map(step => (
 					<div
-					className={`flex flex-col py-[40px] px-[16px] ${
-						step.id === 7 ? 'bg-violet100' : 'bg-white100'
-					} ${step.id <= 10 ? 'border-b border-[#F0F0F0]' : ''}`} // Apply border-b conditionally
-					key={step.id}
-				>
-						<div className='flex items-center rounded-[10px] border border-[#F0F0F0] p-[10px] text-center w-[50px] justify-center'>
+						className={`flex 2xl:w-[50%] flex-col py-[40px] px-[16px] mdl:py-[60px] 2xl:py-[80px] 2xl:px-[70px] mdl:px-[30px] relative ${
+							step.id === 7 ? 'bg-violet100' : 'bg-white100'
+						} ${step.id <= 10 ? 'border-b border-[#F0F0F0]' : ''}   2xl:border-b 2xl:border-r border-[#F0F0F0]`} // Apply border-b conditionally
+						key={step.id}
+					>
+						<div className='flex items-center rounded-[10px] border border-[#F0F0F0] p-[10px] text-center w-[50px] justify-center mdl:w-[65px]'>
 							<p
-								className={` text-[23px] font-medium ${
+								className={` text-[23px] mdl:text-[30px] font-medium ${
 									step.id === 7 ? 'text-white' : 'text-violet100'
 								}`}
 							>
 								{step.number}
 							</p>
 						</div>
-						<div className='flex flex-col mt-[25px]'>
+						<div className='flex flex-col mt-[25px] mdl:mt-[40px]'>
 							<div className='flex flex-row gap-[8px]'>
 								{step.img && (
 									<Image
 										src={step.img}
-										width={16}
-										height={16}
+										width={36}
+										height={36}
 										quality={100}
 										alt={step.imgtitle}
-										className='object-contain'
+										className='object-contain w-[16px] mdl:w-[20px]'
 									/>
 								)}
 
-								<p className='text-violet100 text-[15px] font-semibold font-montserrat'>
+								<p className='text-violet100 text-[15px] font-semibold font-montserrat mdl:text-[18px]'>
 									{step.imgtitle}
 								</p>
 							</div>
 						</div>
-						<div className='mt-[10px] flex flex-col gap-[10px]'>
+						<div className='mt-[10px] flex flex-col gap-[10px] 2xl:mb-[150px] '>
 							<p
 								className={` font-bold text-[23px] ${
 									step.id === 7 ? 'text-white100' : 'text-titleDark'
-								}`}
+								} mdl:text-[45px]`}
 							>
 								{step.title}
 							</p>
 							<p
 								className={` ${
 									step.id === 7 ? 'text-white100' : 'text-titleDark'
-								} tex-[15px] font-medium`}
+								} tex-[15px] font-medium mdl:text-[20px]` }
 							>
 								{step.subtitle}
 							</p>
 						</div>
-						<div className={`mt-[20px] rounded-[20px] py-[25px] px-[20px] flex flex-col gap-[10px] ${step.id === 7 ?`bg-white bg-opacity-15` : 'bg-violet100 bg-opacity-10 '}`}>
+						<div
+							className={`mt-[20px] rounded-[20px] py-[25px] px-[20px] flex flex-col gap-[10px] ${
+								step.id === 7
+									? `bg-white bg-opacity-15`
+									: 'bg-violet100 bg-opacity-10 '
+							} mdl:mt-[40px] 2xl:absolute 2xl:bottom-20 2xl:w-[80%]`}
+						>
 							<p
 								className={`text-[20px] font-semibold  font-montserrat ${
 									step.id === 7 ? 'text-white' : 'text-violet100'
-								}`}
+								} mdl:text-[25px]`}
 							>
 								{step.sel}
 							</p>
 							<p
 								className={`text-[15px]  font-medium font-montserrat leading-[19px] ${
 									step.id === 7 ? 'text-white' : 'text-violet100'
-								}`}
+								} mdl:text-[20px] 2xl:leading-[25px] 3xl:leading-[30px]`}
 							>
 								{step.selTitle}
 							</p>
