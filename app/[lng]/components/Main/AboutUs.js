d375ac92d0df60153452f8987394aa3b22b7d1d5
tarpeeton <146853258+tarpeeton@users.noreply.github.com>
2024-09-25 18:37:07 +0500

    Main About Image

--- app/[lng]/components/Main/AboutUs.js
+++ app/[lng]/components/Main/AboutUs.js
@@ -6,12 +6,12 @@ import Link from 'next/link'
 
 const AboutUs = () => {
 	return (
-		<Link href='/about' className='rounded-[30px] bg-violet100 mdl:rounded-[60px] 2xl:rounded-[100px] px-[20px] py-[30px] w-full mt-[20px] mdl:mt-[25px] 2xl:mt-[30px] mdl:px-[40px] 2xl:px-[70px] mdl:py-[50px] 2xl:py-[80px]  relative mdl:overflow-hidden '>
+		<Link href='/about' className='rounded-[30px] bg-violet100 mdl:rounded-[60px] 2xl:rounded-[100px]  w-full mt-[20px] mdl:mt-[25px] 2xl:mt-[30px] mdl:px-[40px] 2xl:px-[70px] pb-[20px] mdl:py-[50px] 2xl:py-[80px]  relative mdl:overflow-hidden '>
 
-			<div className='mdl:w-[70%]'>
+			<div className='mdl:w-[70%]  px-[20px] pt-[20px]'>
 				<p className='text-[28px] text-white100 font-bold mdl:text-[50px]'>Result Agency — ваш путь к успеху</p>
 			</div>
-			<div className='mt-[24px] mdl:mt-[20px] 2xl:mt-[40px] mdl:w-[80%]'>
+			<div className='mt-[24px] px-[20px] mdl:mt-[20px] 2xl:mt-[40px] mdl:w-[80%]'>
 				<span className='text-[14px] mdl:text-[18px] text-white100 font-medium font-robotoFlex'>
 				Result Agency предлагает комплексный подход к решению маркетинговых задач. Мы специализируемся на разработке комплексных стратегий, направленных на рост вашего медицинского бизнеса.
 				</span>
@@ -19,7 +19,7 @@ const AboutUs = () => {
 			<div className=' rotate-[15%] overflow-hidden relative h-[160px] mt-[-70px]  mdl:absolute mdl:bottom-[-50px] mdl:right-0 mdl:w-[350px] mdl:h-[300px] 2xl:h-[500px] 2xl:w-[500px] '>
 				<Image className='absolute h-[150px] w-[150px] mdl:h-[260px] mdl:w-[300px] right-[-40px] mdl:object-contain 2xl:w-[500px] 2xl:h-[500px]' width={511} height={511} src={aboutUS} quality={100} alt='about us image'/>
 			</div>
-			<div className='w-[80%] mx-auto bg-white rounded-[100px]  py-[20px] px-[30px] text-center  mdl:w-[50%] mdl:mx-0 mdl:mt-[80px] 2xl:w-[20%]'>
+			<div className='w-[80%] px-[20px]  mx-auto bg-white rounded-[100px]  py-[20px]  text-center  mdl:w-[50%] mdl:mx-0 mdl:mt-[80px] 2xl:w-[20%]'>
 			<p  className='  text-violet100 font-bold text-[14px] mdl:text-[18px] text-center bg-white100'>
 				 Подробнее
 			</p>
