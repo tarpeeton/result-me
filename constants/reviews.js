1ad41cbdb94383934785f1440e1954469f7ebd92
tarpeeton <146853258+tarpeeton@users.noreply.github.com>
2024-10-10 12:45:17 +0500

    Reviews

--- constants/reviews.js
+++ constants/reviews.js
@@ -1,69 +1,69 @@
 export const reviewData = [
   {
-    name: { ru: 'Матьякуб Назарович', uz: 'Matyakub Nazarovich', en: '' },
+    name: { ru: 'Матьякуб Назарович', uz: 'Matyakub Nazarovich', en: 'Matyakuub Nazarovich' },
     opucuption: {
       ru: 'Главврач Intermed',
       uz: 'Intermed bosh shifokori',
-      en: ''
+      en: 'Chief Physician, Intermed'
     },
     comment: {
       ru: `Отличная работа! Команда профессионалов создала для нас сайт,
                           который идеально отражает нашу клинику и помогает привлекать новых
                           пациентов.`,
       uz: 'Ajoyib ish! Professionallar jamoasi biz uchun sayt yaratdi, u klinikamizni mukammal tarzda aks ettiryapti va yangi bemorlarni jalb qilishga yordam beryapti.',
-      en: ''
+      en: 'Great work! The team of professionals created a website that perfectly reflects our clinic and helps attract new patients.'
     }
   },
   {
     name: {
       ru: 'Нигора Абдумуталовна',
       uz: 'Nigora Abdumutalovna',
-      en: ''
+      en: 'Nigora Abdumatalovna'
     },
     opucuption: {
       ru: 'Исполнительный директор Prime Medical Center',
       uz: 'Prime Medical Center ijrochi direktori',
-      en: ''
+      en: 'Executive Director, Prime Medical Center'
     },
     comment: {
       ru: `Работа с агентством превзошла все ожидания. Наши социальные сети стали намного активнее, и реклама начала приносить реальные результаты.`,
       uz: 'Agentlik bilan ishlash barcha kutgan natijalardan oshib tushdi. Ijtimoiy tarmoqlarimiz ancha faolroq bo‘ldi , va reklama haqiqiy natijalar keltira  boshladi.',
-      en: ''
+      en: 'Working with the agency exceeded all expectations. Our social media has become significantly more active, and advertising has begun to yield real results.'
     }
   },
   {
-    name: { ru: 'Алишер Бахромович', uz: 'Alisher Baxromovich', en: '' },
-    opucuption: { ru: 'ЛОР-хирург', uz: 'LOR jarrohi', en: '' },
+    name: { ru: 'Алишер Бахромович', uz: 'Alisher Baxromovich', en: 'Alisher Bakharovich' },
+    opucuption: { ru: 'ЛОР-хирург', uz: 'LOR jarrohi', en: 'ENT Surgeon' },
     comment: {
       ru: 'Благодарю за качественный SEO-сервис. Сайт стал заметно лучше виден в поисковиках, и количество пациентов выросло.',
       uz: 'Yuqori sifatli SEO-xizmat uchun rahmat. Sayt qidiruv maydonlarida sezilarli darajada ko‘rinadigan bo‘ldi, va bemorlar soni ortdi.',
-      en: ''
+      en: 'Thank you for the quality SEO service. The website has become noticeably more visible in search engines, and the number of patients has increased.'
     }
   },
   {
-    name: { ru: 'Зокир Атхамович', uz: 'Zokir Atxamovich', en: '' },
+    name: { ru: 'Зокир Атхамович', uz: 'Zokir Atxamovich', en: 'Zokir Akhamovich' },
     opucuption: {
       ru: 'Владелец стоматологии Smile Design',
       uz: 'Smile Design stomatologiyasining egasi',
-      en: ''
+      en: 'Owner, Smile Design Dental Clinic'
     },
     comment: {
       ru: 'Профессиональный подход к созданию бренда и сайта. Клиенты теперь легко находят нас онлайн, и им нравится наш новый имидж.',
       uz: 'Brend va saytni yaratishga professional yondashuv. Mijozlar endi osonlikcha bizni onlayn tarzda topishga va ularga bizning yangi imidjimiz yoqadi.',
-      en: ''
+      en: '"A professional approach to brand and website creation. Clients can now easily find us online, and they love our new image."'
     }
   },
   {
-    name: { ru: 'Санджар Акбарович', uz: 'Sanjar Akbarovich', en: '' },
+    name: { ru: 'Санджар Акбарович', uz: 'Sanjar Akbarovich', en: 'Sanjar Akbarovich' },
     opucuption: {
       ru: 'Врач-психотерапевт',
       uz: 'Psixoterapevt-shifokor',
-      en: ''
+      en: 'Psychotherapist'
     },
     comment: {
       ru: 'Telegram-боты, разработанные для нас, значительно упростили взаимодействие с клиентами. Теперь всё работает быстро и удобно.',
       uz: 'Biz uchun ishlab chiqilgan Telegram-botlar mijozlar bilan aloqani ancha soddalashtirildi. Endi hamma narsa tez va qulay ishlayapti.',
-      en: ''
+      en: 'The Telegram bots developed for us significantly simplified client interactions. Everything now works quickly and conveniently.'
     }
   }
 ]
