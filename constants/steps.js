0fcbda34f95c7020edfd2c133eff26e3585a64fa
tarpeeton <146853258+tarpeeton@users.noreply.github.com>
2024-10-10 12:42:47 +0500

    Steps English

--- constants/steps.js
+++ constants/steps.js
@@ -2,71 +2,71 @@ export const stepsData = [
   {
     id: 1,
     number: '01',
-    title: { ru: 'Консультация', uz: 'Konsultatsiya', en: '' },
+    title: { ru: 'Консультация', uz: 'Konsultatsiya', en: 'Consultation' },
     img: 'https://ucarecdn.com/6c4de8b4-706c-4c1d-a7bd-bc4b38fb4605/-/preview/20x20/',
-    imgtitle: { ru: 'С участием заказчика', uz: 'Mijoz ishtirokida', en: '' },
+    imgtitle: { ru: 'С участием заказчика', uz: 'Mijoz ishtirokida', en: 'Client Involvement' },
     subtitle: {
       ru: 'Консультация позволяет лучше понять задачи и подобрать оптимальные услуги',
       uz: 'Konsultatsiya  vazifalarni yaxshiroq tushunishga  va eng yaxshi xizmatlarni tanlash imkonini beradi',
-      en: ''
+      en: 'The consultation allows us to gain a better understanding of the tasks at hand and to select the optimal services.'
     },
-    sel: { ru: 'Цель', uz: 'Maqsad', en: '' },
+    sel: { ru: 'Цель', uz: 'Maqsad', en: 'Objective' },
     selTitle: {
       ru: 'Определить задачи и предложить решения',
       uz: 'Vazifalarni aniqlang va yechimlarni taklif qiling',
-      en: ''
+      en: 'To define tasks and propose solutions'
     }
   },
   {
     id: 2,
     number: '02',
-    title: { ru: 'Заключение договора', uz: 'Shartnoma tuzish', en: '' },
+    title: { ru: 'Заключение договора', uz: 'Shartnoma tuzish', en: 'Contract Conclusion' },
     img: 'https://ucarecdn.com/6c4de8b4-706c-4c1d-a7bd-bc4b38fb4605/-/preview/20x20/',
-    imgtitle: { ru: 'С участием заказчика', uz: 'Mijoz ishtirokida', en: '' },
+    imgtitle: { ru: 'С участием заказчика', uz: 'Mijoz ishtirokida', en: 'Client Involvement' },
     subtitle: {
       ru: 'Фиксируются условия работы, сроки выполнения, объем услуг и стоимость. Это гарантирует прозрачность и защиту интересов обеих сторон',
       uz: 'Ish shartlari,  bajarilish muddatlari, xizmatlar ko‘lami va narxi belgilanadi. Bu ikki tomonning manfaatlarini himoya qilishni va shaffoflikni kafolatlaydi',
-      en: ''
+      en: 'The terms of cooperation, deadlines, scope of services, and costs are fixed. This ensures transparency and protects the interests of both parties.'
     },
-    sel: { ru: 'Цель', uz: 'Maqsad', en: '' },
+    sel: { ru: 'Цель', uz: 'Maqsad', en: 'Objective' },
     selTitle: {
       ru: 'Зафиксировать условия и защитить интересы',
       uz: 'Shartlarni belgilash va manfaatlarni himoya qilish',
-      en: ''
+      en: 'To establish conditions and protect interests'
     }
   },
   {
     id: 3,
     number: '03',
-    title: { ru: 'Сбор информации', uz: 'Maʼlumot yig‘ish', en: '' },
+    title: { ru: 'Сбор информации', uz: 'Maʼlumot yig‘ish', en: 'Information Gathering' },
     img: 'https://ucarecdn.com/6c4de8b4-706c-4c1d-a7bd-bc4b38fb4605/-/preview/20x20/',
-    imgtitle: { ru: 'С участием заказчика', uz: 'Mijoz ishtirokida', en: '' },
+    imgtitle: { ru: 'С участием заказчика', uz: 'Mijoz ishtirokida', en: 'Client Involvement' },
     subtitle: {
       ru: 'Собирается вся необходимая информация: данные о бизнесе, целевой аудитории, текущие инструменты продвижения, показатели эффективности, конкуренты',
       uz: 'Barcha kerakli maʼlumotlar to‘planadi: biznes, maqsadli auditoriya, olg‘a surish joriy vositalari, samaradorlik ko‘rsatkichlari, raqobatchilar haqidagi maʼlumotlar',
-      en: ''
+      en: 'All necessary information is collected: data about the business, target audience, current promotion tools, performance metrics, and competitors.'
     },
-    sel: { ru: 'Цель', uz: 'Maqsad', en: '' },
+    sel: { ru: 'Цель', uz: 'Maqsad', en: 'Objective' },
     selTitle: {
       ru: 'Получить данные для анализа',
       uz: 'Tahlil va tadqiqot',
-      en: ''
+      en: 'To obtain data for analysis.'
     }
   },
   {
     id: 4,
     number: '04',
-    title: { ru: 'Анализ и исследование', uz: 'Tahlil va tadqiqot', en: '' },
+    title: { ru: 'Анализ и исследование', uz: 'Tahlil va tadqiqot', en: 'Analysis and Research' },
     subtitle: {
       ru: 'Проводится глубокий анализ собранных данных. Это помогает определить сильные и слабые стороны и построить эффективную стратегию',
       uz: 'Yig‘ilgan maʼlumotlar chuqur tahlil qilinadi. Bu kuchli va zaif tomonlarni aniqlashga va samarali strategiyani yaratishga yordam beradi.',
-      en: ''
+      en: 'A thorough analysis of the collected data is conducted. This helps to identify strengths and weaknesses and to build an effective strategy.'
     },
-    sel: { ru: 'Цель', uz: 'Maqsad', en: '' },
+    sel: { ru: 'Цель', uz: 'Maqsad', en: 'Objective' },
     selTitle: {
       ru: 'Определить возможности и точки роста',
       uz: 'Imkoniyatlarni va o‘sish nuqtalarini aniqlash',
-      en: ''
+      en: 'To determine opportunities and growth points.'
     }
   },
   {
@@ -75,33 +75,33 @@ export const stepsData = [
     title: {
       ru: 'Разработка стратегии',
       uz: 'Strategiyani ishlab chiqish',
-      en: ''
+      en: 'Strategy Development'
     },
     subtitle: {
       ru: 'Создается индивидуальная маркетинговая стратегия, основанная на результатах анализа. Включает выбор каналов продвижения, форматов контента и ключевых действий для достижения целей',
       uz: 'Tahlil natijalari asosida individual marketing strategiyasi yaratiladi. Maqsadlarga erishish uchun olg‘a surish kanallarini, kontent formatlarini  va asosiy harakatlarni tanlashni o‘z ichiga oladi',
-      en: ''
+      en: 'An individualized marketing strategy is created based on the results of the analysis. It includes the selection of promotion channels, content formats, and key actions to achieve the goals.'
     },
-    sel: { ru: 'Цель', uz: 'Maqsad', en: '' },
+    sel: { ru: 'Цель', uz: 'Maqsad', en: 'Objective' },
     selTitle: {
       ru: 'Составить план для достижения целей',
       uz: 'Maqsadlarga erishish uchun reja tuzish',
-      en: ''
+      en: 'To formulate a plan for achieving objectives.'
     }
   },
   {
     id: 6,
     number: '06',
-    title: { ru: 'Согласование', uz: 'Shartnoma', en: '' },
+    title: { ru: 'Согласование', uz: 'Shartnoma', en: 'Approval' },
     img: 'https://ucarecdn.com/6c4de8b4-706c-4c1d-a7bd-bc4b38fb4605/-/preview/20x20/',
-    imgtitle: { ru: 'С участием заказчика', uz: '', en: '' },
+    imgtitle: { ru: 'С участием заказчика', uz: 'Mijoz ishtirokida', en: 'Client Involvement' },
     subtitle: {
       ru: 'Подготовленная стратегия обсуждается с заказчиком, при необходимости вносятся коррективы, после чего утверждается план работы',
       uz: 'Tayyorlangan strategiya buyurtmachi bilan muhokama qilinadi, kerak bo‘lsa o‘zgarishlar kiritiladi, so‘ng esa ish rejasi tasdiqlanadi',
-      en: ''
+      en: 'The prepared strategy is discussed with the client, and adjustments are made as necessary, after which the work plan is approved.'
     },
-    sel: { ru: 'Цель', uz: 'Maqsad', en: '' },
-    selTitle: { ru: 'Утвердить план', uz: 'Rejani tasdiqlash', en: '' }
+    sel: { ru: 'Цель', uz: 'Maqsad', en: 'Objective' },
+    selTitle: { ru: 'Утвердить план', uz: 'Rejani tasdiqlash', en: 'To finalize the plan.' }
   },
   {
     id: 7,
@@ -109,18 +109,18 @@ export const stepsData = [
     title: {
       ru: 'Основная работа над проектом',
       uz: 'Loyiha bo‘yicha asosiy ish',
-      en: ''
+      en: 'Main Work on the Project'
     },
     subtitle: {
       ru: 'На данном этапе выполняется вся основная работа: создание контента, разработка IT продуктов, настройка рекламных кампаний, а также создание других материалов проекта',
       uz: 'Bu bosqichda barcha asosiy ishlar amalga oshiriladi: kontent yaratish, IT mahsulotlarini ishlab chiqish, reklama kampaniyalarini tashkil etish, shuningdek loyihaning boshqa materiallarini yaratish',
-      en: ''
+      en: 'At this stage, all main tasks are carried out: content creation, IT product development, campaign setup, and the creation of other project materials.'
     },
-    sel: { ru: 'Цель', uz: 'Maqsad', en: '' },
+    sel: { ru: 'Цель', uz: 'Maqsad', en: 'Objective' },
     selTitle: {
       ru: 'Реализовать все запланированные действия',
       uz: 'Barcha rejalashtirilgan harakatlarni amalga oshirish',
-      en: ''
+      en: 'To implement all planned actions.'
     }
   },
   {
@@ -129,18 +129,18 @@ export const stepsData = [
     title: {
       ru: 'Запуск и реализация',
       uz: 'Ishga tushirish va amalga oshirish',
-      en: ''
+      en: 'Launch and Implementation'
     },
     subtitle: {
       ru: 'Запускается активная фаза проекта: публикация контента, запуск рекламных кампаний, внедрение IT-решений',
       uz: 'Loyihaning faol bosqichi boshlanyapti: kontentni nashr etish, reklama kampaniyalarini boshlash, IT-yechimlarni kiritish',
-      en: ''
+      en: 'The active phase of the project is initiated: content publication, advertising campaign launches, and IT solutions implementation.'
     },
-    sel: { ru: 'Цель', uz: 'Maqsad', en: '' },
+    sel: { ru: 'Цель', uz: 'Maqsad', en: 'Objective' },
     selTitle: {
       ru: 'Обеспечить успешный запуск',
       uz: 'Muvaffaqiyatli ishga tushirishni taʼminlash',
-      en: ''
+      en: 'To ensure a successful launch.'
     }
   },
   {
@@ -149,18 +149,18 @@ export const stepsData = [
     title: {
       ru: 'Мониторинг и корректировка',
       uz: 'Monitoring va sozlash',
-      en: ''
+      en: 'Monitoring and Adjustment'
     },
     subtitle: {
       ru: 'Постоянно отслеживаются результаты, анализируются показатели и эффективность кампаний. В случае необходимости вносятся корректировки для улучшения результатов и повышения отдачи',
       uz: 'Natijalar doimo nazorat qilinadi, kampaniyalarning ko‘rsatkichlari va samaradorligi tahlil qilinadi. Agar kerak bo‘lsa, natijalarni yaxshilash va daromadni oshirish uchun o‘zgarishlar kiritiladi',
-      en: ''
+      en: 'Results are continuously monitored, and metrics and campaign effectiveness are analyzed. Adjustments are made as needed to improve results and increase returns.'
     },
-    sel: { ru: 'Цель', uz: 'Maqsad', en: '' },
+    sel: { ru: 'Цель', uz: 'Maqsad', en: 'Objective' },
     selTitle: {
       ru: 'Оптимизировать процессы для улучшения результатов',
       uz: 'Natijalarni yaxshilash uchun jarayonlarni optimallashtirish',
-      en: ''
+      en: 'To optimize processes for better outcomes.'
     }
   },
   {
@@ -169,18 +169,18 @@ export const stepsData = [
     title: {
       ru: 'Отчетность и подведение итогов',
       uz: 'Hisobot va xulosa chiqarish',
-      en: ''
+      en: 'Reporting and Summarization'
     },
     subtitle: {
       ru: 'Предоставляется отчет о проделанной работе, затраченных средств на рекламные кампании, достигнутых результатов и планах на следующий месяц. Отчет включает рекомендации для дальнейшего улучшения и развития проекта',
       uz: 'Bajarilgan ish, reklama kampaniyalariga sarflangan mablag‘lar, erishilgan natijalar va keyingi oy uchun rejalar to‘g‘risida hisobot taqdim etiladi. Hisobot loyihani yana-da takomillashtirish va rivojlantirish uchun tavsiyalardan iborat',
-      en: ''
+      en: 'A report is provided detailing the work done, funds spent on advertising campaigns, results achieved, and plans for the next month. The report includes recommendations for further improvement and project development.'
     },
-    sel: { ru: 'Цель', uz: 'Maqsad', en: '' },
+    sel: { ru: 'Цель', uz: 'Maqsad', en: 'Objective' },
     selTitle: {
       ru: 'Отчитаться о потраченных средствах на рекламу и выполненных задачах',
       uz: 'Reklama uchun sarflangan mablag‘lar va bajarilgan vazifalar to‘g‘risida hisobot berish',
-      en: ''
+      en: 'To report on advertising expenditures and completed tasks.'
     }
   },
   {
@@ -189,18 +189,18 @@ export const stepsData = [
     title: {
       ru: 'Поддержка и развитие',
       uz: 'Qo‘llab-quvvatlash va rivojlantirish',
-      en: ''
+      en: 'Support and Development'
     },
     subtitle: {
       ru: 'Продолжается поддержка проекта после его завершения. Предлагаются долгосрочные решения для дальнейшего развития бизнеса, улучшения показателей и удержания достигнутых результатов',
       uz: 'Loyiha tugagandan so‘ng uni qo‘llab-quvvatlash davom etadi. Biznesni yana-da rivojlantirish, ko‘rsatkichlarni yaxshilash va erishilgan natijalarni saqlab qolish uchun uzoq muddatli yechimlar taklif etiladi',
-      en: ''
+      en: 'Support for the project continues after its completion. Long-term solutions are offered for further business development, improving performance metrics, and maintaining achieved results.'
     },
-    sel: { ru: 'Цель', uz: 'Maqsad', en: '' },
+    sel: { ru: 'Цель', uz: 'Maqsad', en: 'Objective' },
     selTitle: {
       ru: 'Обеспечить долгосрочный рост проекта',
       uz: 'Loyihaning uzoq muddatli o‘sishini taʼminlash',
-      en: ''
+      en: 'To ensure long-term project growth.'
     }
   }
 ]
