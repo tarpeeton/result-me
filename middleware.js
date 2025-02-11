3ba7fc70a8f501f6a554c53e636f6cd8167ce55b
tarpeeton <tarpeetonteam@gmail.com>
2025-02-11 18:43:05 +1400

    robots  and sitemap

--- middleware.js
+++ middleware.js
@@ -1,45 +1,45 @@
-import { NextResponse } from 'next/server'
-import acceptLanguage from 'accept-language'
-import { fallbackLng, languages, cookieName } from './app/i18n/settings'
+import { NextResponse } from "next/server";
+import acceptLanguage from "accept-language";
+import { fallbackLng, languages, cookieName } from "./app/i18n/settings";
 
-acceptLanguage.languages(languages)
+acceptLanguage.languages(languages);
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js|site.webmanifest).*)'],
-}
+  matcher: [
+    "/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js|site.webmanifest|robots.txt|sitemap.xml).*)",
+  ],
+};
 
 export function middleware(req) {
-  let lng
+  let lng = fallbackLng;
+
   if (req.cookies.has(cookieName)) {
-    const cookieValue = req.cookies.get(cookieName)?.value
+    const cookieValue = req.cookies.get(cookieName)?.value;
     if (cookieValue) {
-      lng = acceptLanguage.get(cookieValue)
+      lng = acceptLanguage.get(cookieValue) || fallbackLng;
     }
-  }
-  
-  if (!lng) {
-    lng = acceptLanguage.get(req.headers.get('Accept-Language'))
+  } else if (req.headers.has("Accept-Language")) {
+    lng = acceptLanguage.get(req.headers.get("Accept-Language")) || fallbackLng;
   }
 
-  if (!lng) {
-    lng = fallbackLng
-  }
-
-  // Redirect if language in path is not supported
   if (
     !languages.some((loc) => req.nextUrl.pathname.startsWith(`/${loc}`)) &&
-    !req.nextUrl.pathname.startsWith('/_next')
+    !req.nextUrl.pathname.startsWith("/_next")
   ) {
-    return NextResponse.redirect(new URL(`/${lng}${req.nextUrl.pathname}`, req.url))
+    return NextResponse.redirect(
+      new URL(`/${lng}${req.nextUrl.pathname}`, req.url)
+    );
   }
 
-  if (req.headers.has('referer')) {
-    const refererUrl = new URL(req.headers.get('referer'))
-    const lngInReferer = languages.find((l) => refererUrl.pathname.startsWith(`/${l}`))
-    const response = NextResponse.next()
-    if (lngInReferer) response.cookies.set(cookieName, lngInReferer)
-    return response
+  if (req.headers.has("referer")) {
+    const refererUrl = new URL(req.headers.get("referer"));
+    const lngInReferer = languages.find((l) =>
+      refererUrl.pathname.startsWith(`/${l}`)
+    );
+    const response = NextResponse.next();
+    if (lngInReferer) response.cookies.set(cookieName, lngInReferer);
+    return response;
   }
 
-  return NextResponse.next()
+  return NextResponse.next();
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-	  domains: ['ucarecdn.com', 'shotambrat.github.io'], // Добавьте домен, откуда будут загружаться изображения
-	  deviceSizes: [640, 750, 1080, 1200, 1920], // Задайте размеры для оптимизации под разные устройства
-	  imageSizes: [16, 32, 48, 64, 96], // Размеры для использования внутри компонента Image
-	  minimumCacheTTL: 60, // Минимальное время кеша (например, 60 секунд)
-	  formats: ['image/avif', 'image/webp'], // Задайте форматы для сжатия изображений
-	},
-	reactStrictMode: true, // Включает строгий режим React
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    domains: ["ucarecdn.com", "shotambrat.github.io"], // Добавьте домен, откуда будут загружаться изображения
+    deviceSizes: [640, 750, 1080, 1200, 1920], // Задайте размеры для оптимизации под разные устройства
+    imageSizes: [16, 32, 48, 64, 96], // Размеры для использования внутри компонента Image
+    minimumCacheTTL: 60, // Минимальное время кеша (например, 60 секунд)
+    formats: ["image/avif", "image/webp"], // Задайте форматы для сжатия изображений
+  },
+  reactStrictMode: false, // Включает строгий режим React
+};
+
+export default nextConfig;
